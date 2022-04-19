7872a658db2382f8ace0634fefa242bfdcc07ca7
Mladen-code <mladen.maksimovic989@gmail.com>
2022-04-19 13:26:29 +0200

    My bookings page tests

--- cypress/integration/my_bookings.js
+++ cypress/integration/my_bookings.js
@@ -2,18 +2,35 @@
 
 import { user } from '../fixtures/config'
 
-describe('Cancelation of pending booking', () => {
+describe('My bookings page', () => {
 
     beforeEach(function() {
         cy.visit('/');
     })
 
-    it.only('Check if proper error message is displayed after cancelation of pending booking', function() {
+    it('Check if link View Voucher is opened in new tab', function() {
         cy.loginUser(user.username, user.password);
         cy.get(".sidebar-menu li a").contains('My Bookings').click({ force: true });
-        cy.contains('tr', 'pending').find('a').contains('View Voucher').click({ force: true });
-        cy.get('[value="Request Cancellation"]').click({ force: true });
-        cy.get('.alert-danger').should('contain.text', "Your cancellation request has been sent successlly. please wait for our response")
+        cy.contains('tr', 'pending').find('a').contains('View Voucher').should('have.attr', 'target"_blank"')
+    });
+
+    it('Check if total bookings number is equal as number of rows in the bookings table', function() {
+        let totalRowNumber;
+        let totalBookingsNumber;
+
+        cy.loginUser(user.username, user.password);
+        cy.get(".sidebar-menu li a").contains('My Bookings').click({ force: true });
+
+        cy.get('tbody > tr').then((row) => {
+            totalRowNumber = row.length;
+        });
+
+        totalBookingsNumber = cy.contains('p', 'Total Bookings').siblings('h4.info__title');
+
+        totalBookingsNumber.invoke('text').then(text => {
+            expect((text).trim()).to.equal('' + totalRowNumber)
+        })
+
     });
 
 })
